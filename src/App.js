6ca99b7972fefe53068662b861b44d9c1036bbb4
errMeep <andypark6004@gmail.com>
2023-06-20 18:57:51 -0400

    Adding points state and images of char

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import './App.css';
+import char1 from './images/char1.webp';
+import char2 from './images/char2.jpeg';
+import char3 from './images/char3.jpeg';
+import char4 from './images/char4.jpeg';
+import char5 from './images/char5.webp';
+import char6 from './images/char6.jpeg';
+import char7 from './images/char7.png';
+import char8 from './images/char8.png';
+import char9 from './images/char9.jpeg';
+import char10 from './images/char10.jpeg';
 
 function App() {
+  const [pointScore, setpointScore] = useState(0);
+  const [highpointScore, sethighpointScore] = useState(0);
+  const [charArray, setcharArray] = useState([
+    char1,
+    char2,
+    char3,
+    char4,
+    char5,
+    char6,
+    char7,
+    char8,
+    char9,
+    char10,
+  ]);
+
   return (
     <div className="App">
-      <Header />
+      <Header pointScore={pointScore} highpointScore={highpointScore} />
       <div className="cardGrid">
         <Card />
         <Card />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import '../App.css';
 
-const Header = () => {
+const Header = (props) => {
+  const { pointScore, highpointScore } = props;
   return (
     <div>
-      <div className="headerTitle">
+      <div className="headerArea">
         <h1>Memory Game!</h1>
+        <div>
+          <div>point = {pointScore}</div>
+          <div>high score = {highpointScore}</div>
+        </div>
       </div>
     </div>
   );
